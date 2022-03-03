616ae56ec88884b14850de03bc399712e73cb5c9
Fahad67 <75368974+Fahad67@users.noreply.github.com>
2022-03-03 12:50:51 +0500

    Refine toaster login

--- src/lib/hook/apiHook.js
+++ src/lib/hook/apiHook.js
@@ -7,21 +7,25 @@ export const useAxiosInterceptor = () => {
   const [isPending, setIsPending] = useState(undefined);
   const [error, setError] = useState(null);
 
+  const pushToast = (method, message, config) => {
+    toast[method](message, {
+      position: config?.tostPosition,
+      autoClose: config?.delay || 5000,
+      hideProgressBar: config?.hideProgressBar,
+      closeOnClick: config.closeOnClick,
+      pauseOnHover: config?.pauseOnHover,
+      draggable: config.draggable,
+      progress: undefined,
+      rtl: false,
+      theme: config.theme,
+    });
+  };
+
   const successHandler = (res, config) => {
     setData(res.data);
     setIsPending(false);
     if (config?.displayToast) {
-      toast.success(config?.message || 'Success', {
-        position: config?.tostPosition,
-        autoClose: config?.delay || 5000,
-        hideProgressBar: config?.hideProgressBar,
-        closeOnClick: config.closeOnClick,
-        pauseOnHover: config?.pauseOnHover,
-        draggable: config.draggable,
-        progress: undefined,
-        rtl: false,
-        theme: config.theme,
-      });
+      pushToast('success', config?.successMessage || res.data.message, config);
     }
   };
 
@@ -29,17 +33,39 @@ export const useAxiosInterceptor = () => {
     setError(error);
     setIsPending(false);
     if (config?.displayToast) {
-      toast.error(config?.message || JSON.stringify(error.messages[0]), {
-        position: config?.tostPosition,
-        autoClose: config?.delay || 5000,
-        hideProgressBar: config?.hideProgressBar,
-        closeOnClick: config.closeOnClick,
-        pauseOnHover: config?.pauseOnHover,
-        draggable: config.draggable,
-        progress: undefined,
-        rtl: false,
-        theme: config.theme,
-      });
+      if (
+        error &&
+        error.messages &&
+        error.messages[0] &&
+        error.messages[0].status == 500
+      ) {
+        pushToast(
+          'error',
+          'Something went wrong please contact system admin',
+          config
+        );
+      }
+      if (
+        error &&
+        error.messages &&
+        error.messages[0].status !== 500 &&
+        error.messages[0].data &&
+        error.messages[0].data.errors &&
+        Object.keys(error.messages[0].data.errors).length > 0
+      ) {
+        error.messages[0].data.errors.map(err =>
+          pushToast('error', err || 'Something went wrong', config)
+        );
+      }
+      if (
+        error &&
+        error.messages &&
+        error.messages[0].status !== 500 &&
+        error.messages[0].data &&
+        error.messages[0].data.message
+      ) {
+        pushToast('error', error.messages[0].data.message, config);
+      }
     }
   };
 
@@ -47,27 +73,20 @@ export const useAxiosInterceptor = () => {
     return new Promise((resolve, reject) => {
       try {
         setIsPending(true);
-        if (config?.method !== 'get' && config?.method !== 'delete') {
-          return http[config?.method](config?.url, config?.data)
-            .then(res => {
-              successHandler(res, config);
-              resolve(res.data);
-            })
-            .catch(error => {
-              errorHandler(error, config);
-              reject(false);
-            });
-        } else {
-          http[config?.method](config?.url)
-            .then(res => {
-              successHandler(res, config);
-              resolve(res.data);
-            })
-            .catch(error => {
-              errorHandler(error, config);
-              reject(false);
-            });
-        }
+        return http({
+          url: config?.url,
+          method: config?.method,
+          data: config?.data ?? config?.data,
+          params: config?.params ?? config?.params,
+        })
+          .then(res => {
+            successHandler(res, config);
+            resolve(res.data);
+          })
+          .catch(error => {
+            errorHandler(error, config);
+            reject(error);
+          });
       } catch (error) {
         reject(false);
       }
